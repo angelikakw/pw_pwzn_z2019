fcd1270070d4dbfd4b46038b68ba73ba7f6d181c
angelikakwiatek <eg.angelika@gmail.com>
2019-10-11 07:26:02 +0200

    Zmiana

--- lab_1/task_2.py
+++ lab_1/task_2.py
@@ -1,5 +1,18 @@
 def task_2():
-    return ''
+    napis = '*'
+    print(napis)
+    for i in range(1, 5):
+        napis = napis + " *"
+        print(napis)
+
+    napis2 = "\n"
+    for i in range(1,5):
+        napis2 = (5-i) * "* "
+        print(napis2)
+
+    return napis
+
+
 
 
 assert task_2() == '''
