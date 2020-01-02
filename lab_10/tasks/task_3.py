cb80b222766b2984c3e89e04236d0c8fa83e6c98
angelikakwiatek <eg.angelika@gmail.com>
2020-01-02 23:41:42 +0100

    Task 1, 2, 3 - finished

--- lab_10/tasks/task_3.py
+++ lab_10/tasks/task_3.py
@@ -1,5 +1,7 @@
 import filecmp
 import pathlib
+from glob import glob
+from os.path import join
 from typing import Union
 
 import pandas as pd
@@ -11,7 +13,22 @@ API_URL = 'https://www.metaweather.com/api/'
 def concat_data(
         path: Union[str, pathlib.Path],
 ):
-    pass
+    all_necessary_data = []
+    for file_path in glob(join(path, '*.csv')):
+        data_in_file = pd.read_csv(file_path)
+        filter_data = pd.to_datetime(data_in_file['created']).dt.date == \
+                   pd.to_datetime(data_in_file['applicable_date']).dt.date
+        all_necessary_data.append(data_in_file[filter_data])
+    all_data_concat = pd.concat(all_necessary_data)
+    all_data_concat.rename(columns={'the_temp': 'temp'}, inplace=True)
+    selected_columns = pd.DataFrame(all_data_concat,
+                             columns=['created', 'min_temp', 'temp', 'max_temp',
+                                      'air_pressure', 'humidity', 'visibility',
+                                      'wind_direction_compass', 'wind_direction',
+                                      'wind_speed'])
+    selected_columns.sort_values(['created'], inplace=True)
+    selected_columns['created'] = pd.to_datetime(selected_columns['created']).dt.strftime('%Y-%m-%dT%H:%M')
+    selected_columns.to_csv('{}.csv'.format(path), index=False)
 
 
 if __name__ == '__main__':
