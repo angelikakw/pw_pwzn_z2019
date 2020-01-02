ad9eb918d7427e3066e8ae670fc793761d4c9412
angelikakwiatek <eg.angelika@gmail.com>
2020-01-02 22:34:51 +0100

    Zadania (zad.1,2)

--- lab_10/tasks/task_1.py
+++ lab_10/tasks/task_1.py
@@ -1,8 +1,28 @@
+from urllib.parse import urljoin
 import requests
+
+API_URL = 'https://www.metaweather.com/api/'
 
 
 def get_cities_woeid(query: str, timeout: float = 5.):
-    pass
+    location_url = urljoin(API_URL, 'location/search')
+    response = requests.get(location_url, params=dict(query=query), timeout=timeout)
+
+    if response.status_code >= 400:
+        raise requests.exceptions.HTTPError()
+
+    try:
+        response_json = response.json()
+    except ValueError:
+        raise RuntimeError('No valid json found.')
+
+    result = {}
+    for record in response_json:
+        if 'title' not in record or 'woeid' not in record:
+            raise RuntimeError('No title or woeid.')
+        result[record['title']] = record['woeid']
+
+    return result
 
 
 if __name__ == '__main__':
--- lab_10/tasks/task_2.py
+++ lab_10/tasks/task_2.py
@@ -1,6 +1,11 @@
+import csv
 import pathlib
+from urllib.parse import urljoin
 from typing import Optional, Union, List
-
+from calendar import monthrange
+from os.path import isdir, join
+from os import makedirs, getcwd
+import requests
 
 API_URL = 'https://www.metaweather.com/api/'
 
@@ -10,7 +15,45 @@ def get_city_data(
         path: Optional[Union[str, pathlib.Path]] = None,
         timeout: float = 5.
 ) -> (str, List[str]):
-    pass
+
+    name_of_dir = '{}_{}_{:02}'.format(woeid, year, month)
+    full_name_of_dir = join(path or getcwd(), name_of_dir)
+    if not isdir(full_name_of_dir):
+        makedirs(full_name_of_dir)
+
+    days_in_month = monthrange(year, month)[1]
+    all_files = []
+    for day in range(1, days_in_month+1):
+
+        location_url = urljoin(API_URL, f'location/{woeid}/{year}/{month}/{day}')
+        records = make_request(location_url, timeout)
+        if not records:
+            continue
+
+        name_of_file = '{}_{:02}_{:02}.csv'.format(year, month, day)
+        all_files.append(name_of_file)
+        full_name_of_file = join(full_name_of_dir, name_of_file)
+
+        with open(full_name_of_file, 'w', newline='') as output_file:
+            writer = csv.writer(output_file)
+            writer.writerow(records[0].keys())
+            for row in records:
+                writer.writerow(row.values())
+    return full_name_of_dir, all_files
+
+
+def make_request(location_url, timeout):
+    response = requests.get(location_url, timeout=timeout)
+
+    if response.status_code >= 400:
+        raise requests.exceptions.HTTPError()
+
+    try:
+        response_json = response.json()
+    except ValueError:
+        raise RuntimeError('No valid json found.')
+
+    return response_json
 
 
 if __name__ == '__main__':
