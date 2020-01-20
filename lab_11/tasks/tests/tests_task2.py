d2bf5f31d6e4ed75c265fc8f3bebdf7ea92df0f4
angelikakwiatek <eg.angelika@gmail.com>
2020-01-20 23:49:51 +0100

    Zadania (zad.1,2)

--- lab_11/tasks/tests/tests_task2.py
+++ lab_11/tasks/tests/tests_task2.py
@@ -1,4 +1,6 @@
+import json
 from unittest.mock import Mock, patch
+from urllib.parse import urljoin
 
 import pytest
 import requests
@@ -12,8 +14,33 @@ from lab_11.tasks.tools.metaweather import (
 API_URL = 'https://www.metaweather.com/api/'
 
 
-def test_url():
+def test_war():
     with requests_mock.Mocker() as m:
-        m.get(API_URL, text='resp')
-        requests.get(API_URL).text
+        location_url = urljoin(API_URL, 'location/search')
 
+        m.get(location_url, text='[{"title":"Warsaw","location_type":"City","woeid":523920,"latt_long":"52.235352,21.009390"}'
+                                 ',{"title":"Newark","location_type":"City","woeid":2459269,"latt_long":"40.731972,-74.174179"}]'
+              , status_code=200)
+
+        assert get_cities_woeid('War') == {
+        'Warsaw': 523920,
+        'Newark': 2459269,
+    }
+
+
+def test_warszawa():
+    with requests_mock.Mocker() as m:
+        location_url = urljoin(API_URL, 'location/search')
+
+        m.get(location_url, text='{}', status_code=200)
+        assert get_cities_woeid('Warszawa') == {}
+
+
+def test_timeout():
+    try:
+        with requests_mock.Mocker() as m:
+            location_url = urljoin(API_URL, 'location/search')
+            m.get(location_url, exc=requests.exceptions.Timeout)
+            get_cities_woeid('Warsaw', 5.)
+    except Exception as exc:
+        assert isinstance(exc, requests.exceptions.Timeout)
