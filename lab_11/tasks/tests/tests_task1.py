5ba4d4b057c3c1cb7ec1a1b56790e17e24fadd87
angelikakwiatek <eg.angelika@gmail.com>
2020-01-20 18:13:23 +0100

    Zadania (zad.1)

--- lab_11/tasks/tests/tests_task1.py
+++ lab_11/tasks/tests/tests_task1.py
@@ -7,3 +7,54 @@ from lab_11.tasks.tools.calculator import (
     NotNumberArgument,
     WrongOperation,
 )
+
+
+@pytest.fixture
+def calculator():
+    calculator = Calculator()
+    return calculator
+
+
+@pytest.mark.parametrize(
+    ('op', 'arg1', 'arg2', 'res'),
+    [
+        ('+', 1, 2, 3),
+        ('-', 4, 2, 2),
+        ('*', 10, 4, 40),
+        ('/', 6, 3, 2)
+    ]
+)
+def test_run(calculator, op, arg1, arg2, res):
+    assert calculator.run(op, arg1, arg2) == res
+
+
+def test_memorize(calculator):
+    calculator.run('-', 6, 4)
+    calculator.memorize()
+    assert calculator.memory == 2
+    assert calculator.run('+', 2) == 4
+
+
+def test_wrong_operation(calculator):
+    with pytest.raises(WrongOperation):
+        calculator.run('x', 4, 5)
+
+
+def test_not_number_argument(calculator):
+    with pytest.raises(NotNumberArgument):
+        calculator.run('+', 4, 'b')
+
+
+def test_calculator_error(calculator):
+    with pytest.raises(CalculatorError):
+        calculator.run('/', 9, 0)
+
+
+def test_empty_in_memory(calculator):
+    with pytest.raises(EmptyMemory):
+        calculator.in_memory()
+
+
+def test_empty_memory(calculator):
+    with pytest.raises(EmptyMemory):
+        calculator.run('/', 9)
--- lab_11/tasks/tests/tests_task2.py
+++ lab_11/tasks/tests/tests_task2.py
@@ -1,5 +1,10 @@
 import pytest
+import requests
+import requests_mock
 
 from lab_11.tasks.tools.metaweather import (
-    get_metaweather
+    get_metaweather,
+    get_cities_woeid
 )
+
+
